b67e0be527e8c7253409c6390a0c1c63772e426a
Mustafa Khan <makman09@gmail.com>
2016-08-24 00:57:19 -0700

    implemented feature matrix

--- ml-model.py
+++ ml-model.py
@@ -1,3 +1,17 @@
 import numpy
 import csv
 
+# initialize theta to zeros
+theta = numpy.matrix(str(0) + ';' + str(0))
+
+with open('cleanData.csv', 'rb') as csvfile:
+	reader = csv.reader(csvfile)
+
+	feature_matrix = []
+
+	for row in reader:
+		if (row[0] == 'SQFT'):
+			continue
+		feature_matrix.append([1,int(row[0])])
+
+	print numpy.matrix(feature_matrix)

b28e65e189131a09d57f094ccb452cf1c6ab2027
Mustafa Khan <makman09@gmail.com>
2016-08-24 01:08:35 -0700

    almost done with cost function implementation

--- ml-model.py
+++ ml-model.py
@@ -1,17 +1,32 @@
 import numpy
 import csv
 
+'''
+Cost function
+@param input {matrix} 
+@param output {matrix}
+@param theta {matrix}
+'''
+def costFunction(inputX, outputY, theta):
+	print inputX.dot(theta)
+
 # initialize theta to zeros
 theta = numpy.matrix(str(0) + ';' + str(0))
 
+# initialize feature matrix and output matrix from datapoints of csv file
+feature_matrix = []
+output_matrix = []
+
 with open('cleanData.csv', 'rb') as csvfile:
 	reader = csv.reader(csvfile)
-
-	feature_matrix = []
 
 	for row in reader:
 		if (row[0] == 'SQFT'):
 			continue
 		feature_matrix.append([1,int(row[0])])
+		output_matrix.append([int(row[1])])
 
-	print numpy.matrix(feature_matrix)
+	feature_matrix = numpy.matrix(feature_matrix)
+	output_matrix = numpy.matrix(output_matrix)
+
+costFunction(feature_matrix, output_matrix, theta)
